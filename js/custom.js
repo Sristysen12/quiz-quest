f00ae743690b56c575d82c1ee9420d851081bb85
[Sristy] <[sristysen3@gmail.com]>
2025-07-29 00:08:31 +0530

    Added js for some functionalities.

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var skip = document.getElementById('Skip');
+var score = document.getElementById('score');
+var total_score = document.getElementById('total_score');
+var countdown = document.getElementById('countdown');
+
+
+
